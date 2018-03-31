2f644966af9e20b53cc6c35519fe01480644645e
Ryan Dawson <ryan.dawson@alfresco.com>
2018-03-31 17:46:36 +0100

    add megatron

---
+++ megatron/src/main/java/org/transformers/megatron/Controller.java
@@ -0,0 +1,38 @@
+package org.transformers.megatron;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    private final String name="Megatron";
+    private final String allegiance="Decepticon";
+    @Value("${transformer.mode:disguised}")
+    private String mode;
+
+    @Value("${transformer.disguised:TRUCK}")
+    private String disguised;
+
+    @Value("${transformer.robot:ROBOT}")
+    private String robot;
+
+    @GetMapping
+    public String transformer(){
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("<h1>");
+        stringBuilder.append("Name: ").append(name).append("<br/>");
+        stringBuilder.append("Allegiance: ").append(allegiance).append("<br/>");
+        stringBuilder.append("Mode: ").append(mode).append("<br/>");
+        stringBuilder.append("</h1>");
+
+        if("robot".equalsIgnoreCase(mode)){
+            stringBuilder.append(robot);
+        } else if("disguised".equalsIgnoreCase(mode)){
+            stringBuilder.append(disguised);
+        }
+
+        return stringBuilder.toString();
+    }
+}
---
+++ megatron/src/main/java/org/transformers/megatron/MegatronApplication.java
@@ -0,0 +1,12 @@
+package org.transformers.megatron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegatronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegatronApplication.class, args);
+	}
+}
