be307622394f1ae4ef926633c2b3466ec0dc6478
Ryan Dawson <ryan.dawson@alfresco.com>
2018-03-31 17:30:16 +0100

    initial commit - just optimus

---
+++ optimus-prime/src/main/java/org/transformers/optimusprime/OptimusPrimeApplication.java
@@ -0,0 +1,12 @@
+package org.transformers.optimusprime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimusPrimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptimusPrimeApplication.class, args);
+	}
+}
